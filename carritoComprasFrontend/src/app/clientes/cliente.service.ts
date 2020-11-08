4047412c98efb673ffb8f6ff63d2d0b72f424c4e
J0hann48 <joahnn.1996@gmail.com>
2020-11-08 18:19:41 -0500

    Commit inicial - proyecto frontend

---
+++ carritoComprasFrontend/src/app/clientes/cliente.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { CLIENTES } from './clientes.json';
+import { Cliente } from './cliente';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class ClienteService {
+
+  private urlEndpoint: string = 'http://localhost:8080/api/clientes';
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getClientes(): Observable<Cliente[]>{
+
+    return this.http.get(this.urlEndpoint).pipe(
+      map(response => response as Cliente[])
+    );
+  }
+
+  create(cliente: Cliente): Observable<any>{
+    return this.http.post<any>(this.urlEndpoint, cliente, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes']);
+        Swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(e);
+      })
+    );
+  }
+  
+  getCliente(id): Observable<Cliente>{
+    return this.http.get<Cliente>(`${this.urlEndpoint}/${id}`).pipe(
+      map((response: any) => response.cliente as Cliente),
+      catchError(e => {
+        this.router.navigate(['/clientes']);
+        Swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(e);
+      })
+    );
+  }
+
+  update(cliente: Cliente): Observable<any>{
+    return this.http.put<any>(`${this.urlEndpoint}/${cliente.id}`, cliente, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes']);
+        Swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(e);
+      })
+    );
+  }
+
+  delete(id: number): Observable<Cliente>{
+    return this.http.delete<Cliente>(`${this.urlEndpoint}/${id}`, {headers: this.httpHeaders}).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes']);
+        Swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(e);
+      })
+    );
+  }
+}
---
+++ carritoComprasFrontend/src/app/clientes/clientes.json.ts
@@ -0,0 +1,13 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Johann', apellido: 'Torres', email: 'asds@sssd.com', createAt: '2020-01-01'},
+    {id: 2, nombre: 'Carlos', apellido: 'Mosquera', email: 'asds@sssd.com', createAt: '2020-01-01'},
+    {id: 3, nombre: 'Laura', apellido: 'Mina', email: 'asds@sssd.com', createAt: '2020-01-01'},
+    {id: 4, nombre: 'Miguel', apellido: 'Ramirez', email: 'asds@sssd.com', createAt: '2020-01-01'},
+    {id: 5, nombre: 'Pedro', apellido: 'Pedroza', email: 'asds@sssd.com', createAt: '2020-01-01'},
+    {id: 6, nombre: 'Sofia', apellido: 'Pinzón', email: 'asds@sssd.com', createAt: '2020-01-01'},
+    {id: 7, nombre: 'Viviana', apellido: 'Valero', email: 'asds@sssd.com', createAt: '2020-01-01'},
+    {id: 8, nombre: 'Carmen', apellido: 'Rodriguez', email: 'asds@sssd.com', createAt: '2020-01-01'},
+    {id: 9, nombre: 'Daniela', apellido: 'Guzman', email: 'asds@sssd.com', createAt: '2020-01-01'},    
+  ];
